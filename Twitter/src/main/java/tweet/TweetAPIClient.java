57ecfc4dd09cb83724e140d6952c73f1e09b64f5
sadiaiguerguit <sidialiifri82@gmail.com>
2020-12-08 21:37:56 -0500

    RestAPI done

--- Twitter/src/main/java/tweet/TweetAPIClient.java
+++ Twitter/src/main/java/tweet/TweetAPIClient.java
@@ -1,11 +1,14 @@
 package tweet;
 
 import base.RestAPI;
+import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.response.ValidatableResponse;
 
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.concurrent.TimeUnit;
 
 import static io.restassured.RestAssured.given;
@@ -19,6 +22,49 @@ public class TweetAPIClient extends RestAPI {
     // https://developer.twitter.com/en/docs/twitter-api/v1/tweets/timelines/api-reference/get-statuses-home_timeline
     private final String GET_USER_TWEET_ENDPOINT = "/statuses/home_timeline.json";
     private final String CREATE_RETWEET_ENDPOINT = "/statuses/retweet/:id.json";
+    private final String POST_STATUES_ENDPOINT = "/statuses/update.json";
+    private final String DELETE_STATUES_ENDPOINT = "/statuses/destroy/";
+    private final String ID_JSON = ".json";
+    private final String GET_STATUES_ENDPOINT = "/statuses/show/";
+    private final String JSON_ID = ".json";
+    private final String GET_LOOKUP_ENDPOINT = "/statuses/lookup/";
+    private final String JSON_ID1 = ".json";
+    private final String POST_STATUES_RETWEET = "/statuses/retweet/";
+    private final String Id_JSON = ".json";
+    private final String POST_STATUES_UNRETWEET = "/statuses/unretweet/";
+    private final String Id_JSON_ENDPOINT = ".json";
+    private final String End_Statues_Retweet = "/statuses/retweets/";
+    private final String gET_JSON_ID = ".json";
+    private final String Get_STATUES_RETWEET = "/statuses/retweets_of_me.json";
+    private final String POST_FAVORITE_CREATE = "/favorites/create.json";
+    private final String POST_DELETE_FAVORITE = "/favorites/destroy.json";
+    private final String GET_FAVORITE_LIST = "/favorites/list.json";
+    private final String POST_STATUES_UPDATE = "/statuses/update_with_media.json";
+    private final String GET_STATUES_TIMELine = "/statuses/home_timeline.json";
+    private final String GET_STATUES_USER = "/statuses/user_timeline.json";
+    private final String POST_STATUES_FILTER = "/statuses/filter.json";
+    private final String get_STATUES_SIMPLE = "/statuses/sample.json";
+    private final String get_COLLECTIONS_ENTRIES = "/collections/entries.json";
+    private final String CREATE_MEDIA_ENDPOINT = "/media/upload.json";
+    private final String CREATE_MESSAGE_ENDPOINT = "/direct_messages/events/new.json";
+    private final String GET_ACCOUNT_SETTING = "/account/settings.json";
+    private final String GET_ACCOUNT_VERIFY = "/account/verify_credentials.json";
+    private final String GET_UsersProfile_BANNER = "/users/profile_banner.json";
+    private final String POST_Account_Remove = "/account/remove_profile_banner.json";
+    private final String POST_Account_Settings = "/account/settings.json";
+    private final String POST_Account_UPDATE = "/account/update_profile.json";
+    private final String POST_SAVE_SEARCH = "/saved_searches/list.json";
+    private final String GET_SAVE_SEARCHShow = "/saved_searches/show/";
+    private final String GET_SAVE_JSON = ".json";
+    private final String POST_SAVED_SEARCH = "/saved_searches/create.json";
+    private final String POST_SAVED_SEARCH_DESTROY = "/saved_searches/destroy/";
+    private final String POST_ID_JSON = ".json";
+    private final String POST_PROFILE_IMAGE_ENDPOINT = "/account/update_profile_image.json";
+    private final String POST_MEDIA_ENDPOINT = "/media/upload.json";
+    private final String POST_WELCOME_MESSAGE = "/direct_messages/welcome_messages/new.json";
+
+
+    String jsonPath = "Twitter/JSonFile/WelcomMessage.json";
 
 
     public ValidatableResponse getUserTimeTweet() {
@@ -75,4 +121,263 @@ public class TweetAPIClient extends RestAPI {
                 .then();
     }
 
+    public ValidatableResponse postStatus(String Status) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("status", Status)
+                .when().post(this.baseUrl + this.POST_STATUES_ENDPOINT)
+                .then().log().all().assertThat().statusCode(200);
     }
+
+    public ValidatableResponse deleteStatus(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id_str", tweetId)
+                .when().post(this.baseUrl + this.DELETE_STATUES_ENDPOINT + tweetId + ID_JSON)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse getStatues(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id_str", tweetId)
+                .when().get(this.baseUrl + this.GET_STATUES_ENDPOINT + tweetId + JSON_ID)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse getStatuesLookup(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id_str", tweetId)
+                .when().get(this.baseUrl + this.GET_LOOKUP_ENDPOINT + tweetId + JSON_ID1)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse postStatuesRetweet(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("user", tweetId)
+                .when().post(this.baseUrl + this.POST_STATUES_RETWEET + tweetId + Id_JSON)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+
+    public ValidatableResponse postUnRetweet(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id_str", tweetId)
+                .when().post(this.baseUrl + this.POST_STATUES_UNRETWEET + tweetId + Id_JSON_ENDPOINT)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse getStatusRetweet(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id_str", tweetId)
+                .when().get(this.baseUrl + this.End_Statues_Retweet + tweetId + gET_JSON_ID)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse getStatusRetweetOfMe(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id_str", tweetId)
+                .when().get(this.baseUrl + this.Get_STATUES_RETWEET)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+
+    public ValidatableResponse postCreateFavorite(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", tweetId)
+                .when().post(this.baseUrl + this.POST_FAVORITE_CREATE)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse postDeleteFavorite(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", tweetId)
+                .when().post(this.baseUrl + this.POST_DELETE_FAVORITE)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+
+    public ValidatableResponse getFavoriteLists(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_FAVORITE_LIST)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+
+    public ValidatableResponse postStatuesUpdate(String status) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("status", status)
+                .when().post(this.baseUrl + this.POST_STATUES_UPDATE)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse getStatuesHome(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_STATUES_TIMELine)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse getStatuesMentions() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_STATUES_TIMELine)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse getStatuesUserTimeLine() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_STATUES_USER)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse getFilter() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().post(this.baseUrl + this.POST_STATUES_FILTER)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+
+    public ValidatableResponse getStatuesSimple() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.get_STATUES_SIMPLE)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+    public ValidatableResponse getCollectionsEntries(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", tweetId)
+                .when().get(this.baseUrl + this.get_COLLECTIONS_ENTRIES)
+                .then().log().all().assertThat().statusCode(200);
+    }
+
+
+    public ValidatableResponse createMessage() throws FileNotFoundException {
+        FileInputStream jsonMessage = new FileInputStream("C:\\Users\\Owner\\IdeaProjects\\RestAPIAutomation_Team2\\Twitter\\JSonFile\\JsonMessage.json");
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .log().all()
+                .accept(ContentType.JSON)
+                .header("Content_Type", "application/json")
+                .contentType(ContentType.JSON)
+                .body(jsonMessage)
+                .when().post(this.baseUrl + this.CREATE_MESSAGE_ENDPOINT)
+                .then().log().all();
+    }
+
+
+    public ValidatableResponse createSecondMessage() throws FileNotFoundException {
+        FileInputStream jsonMessage = new FileInputStream("C:\\Users\\Owner\\IdeaProjects\\RestAPIAutomation_Team2\\Twitter\\JSonFile\\JsonSecondMessage.json");
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .log().all()
+                .accept(ContentType.JSON)
+                .header("Content_Type", "application/json")
+                .contentType(ContentType.JSON)
+                .body(jsonMessage)
+                .when().post(this.baseUrl + this.CREATE_MESSAGE_ENDPOINT)
+                .then().log().all();
+    }
+
+
+    public ValidatableResponse getAccountSetting() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                //   .queryParam("id", tweetId)
+                .when().get(this.baseUrl + this.GET_ACCOUNT_SETTING)
+                .then();
+    }
+
+    public ValidatableResponse getAccountVerifyCredentials() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().get(this.baseUrl + this.GET_ACCOUNT_VERIFY)
+                .then().log().all();
+    }
+
+    public ValidatableResponse getUsersProfileBanner(Long id) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("user_id", id)
+                .when().get(this.baseUrl + this.GET_UsersProfile_BANNER)
+                .then();
+    }
+
+    public ValidatableResponse postRemoveAccount() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().post(this.baseUrl + this.POST_Account_Remove)
+                .then().log().all();
+    }
+
+
+    public ValidatableResponse postAccountSetting() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().post(this.baseUrl + this.POST_Account_Settings)
+                .then().log().all();
+    }
+
+
+    public ValidatableResponse postAccountUpdateProfile() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .when().post(this.baseUrl + this.POST_Account_UPDATE)
+                .then().log().all();
+    }
+
+
+    public ValidatableResponse postSavedSearchesList() {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                // .queryParam("banner", banner)
+                .when().get(this.baseUrl + this.POST_SAVE_SEARCH)
+                .then().log().all();
+    }
+
+
+    public ValidatableResponse getSavedSearchesShow(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", tweetId)
+                .when().get(this.baseUrl + this.GET_SAVE_SEARCHShow + tweetId + GET_SAVE_JSON)
+                .then().log().all();
+    }
+
+
+    public ValidatableResponse postSavedSearch(String Query) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("query", Query)
+                .when().post(this.baseUrl + this.POST_SAVED_SEARCH)
+                .then().log().all();
+    }
+
+
+    public ValidatableResponse postSavedSearchesDelete(String tweetId) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .queryParam("id", tweetId)
+                .when().post(this.baseUrl + this.POST_SAVED_SEARCH_DESTROY + tweetId + POST_ID_JSON)
+                .then().log().all();
+    }
+
+    public ValidatableResponse uploadProfileImage(String image) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("image", image)
+                .when().post(this.baseUrl + this.POST_PROFILE_IMAGE_ENDPOINT)
+                .then().log().all();
+    }
+
+    public ValidatableResponse uploadPostImage(String base64) {
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .param("media", base64)
+                .param("shared", "1")
+                .when().post(this.uploadBase + this.POST_MEDIA_ENDPOINT)
+                .then().log().all();
+    }
+
+
+    public ValidatableResponse createWelcomeMessage(String s, String flowerImage) throws FileNotFoundException {
+        FileInputStream inputStream = new FileInputStream("C:\\Users\\Owner\\IdeaProjects\\RestAPIAutomation_Team2\\Twitter\\JSonFile\\WelcomMessage.json");
+        return given().auth().oauth(this.apiKey, this.apiSecretKey, this.accessToken, this.accessTokenSecret)
+                .log().all()
+                .accept(ContentType.JSON)
+                .header("Content-Type", "application/json")
+                .contentType(ContentType.JSON)
+                .body(inputStream)
+                .when().post(this.baseUrl + this.POST_WELCOME_MESSAGE)
+                .then();
+    }
+
+
+
+
+}
+
+
+
+
--- Twitter/src/test/java/tweetTest/TweetAPIClientTest.java
+++ Twitter/src/test/java/tweetTest/TweetAPIClientTest.java
@@ -4,13 +4,18 @@ import io.restassured.response.ValidatableResponse;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import sadiawelcom.WelcomeMessageSadia;
+import tweet.BaseUpload;
 import tweet.TweetAPIClient;
+import tweet.UploadImage;
 
+import java.io.FileNotFoundException;
 import java.util.UUID;
 
 public class TweetAPIClientTest {
 
     private TweetAPIClient tweetAPIClient;
+    private TweetAPIClient tweetAPIClientSadia;
 
 
     @BeforeClass
@@ -30,7 +35,8 @@ public class TweetAPIClientTest {
 
     @Test(enabled = true)
     public void testUserCanNotTweetTheSameTweetTwiceInARow() {
-        String tweet = "We are learning Rest API Automation with Mahmud " + UUID.randomUUID().toString();;
+        String tweet = "We are learning Rest API Automation with Mahmud " + UUID.randomUUID().toString();
+        ;
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         response.statusCode(200);
         System.out.println(response.extract().body().asPrettyString());
@@ -56,24 +62,202 @@ public class TweetAPIClientTest {
     }
 
 
-    @Test(enabled = true)
+    @Test
     public void testResponseTime() {
         ValidatableResponse response = this.tweetAPIClient.responseTime();
     }
 
-    @Test(enabled = true)
+    @Test
     public void testHeaderValue() {
         this.tweetAPIClient.headerValue();
     }
 
-    @Test(enabled = true)
+    @Test
     public void testPropertyFromResponse() {
         String tweet = "Hello everyone" + UUID.randomUUID().toString();
         ValidatableResponse response = this.tweetAPIClient.createTweet(tweet);
         System.out.println(response.extract().body().asPrettyString().contains("id"));
     }
 
+    @Test
+    public void testPostStatus() {
+        this.tweetAPIClient.postStatus("forever sadia");
+    }
 
+    @Test
+    public void testDeleteStatus() {
+        this.tweetAPIClient.deleteStatus("1335675369614176256");
+    }
+
+    @Test
+    public void testGetStatus() {
+        this.tweetAPIClient.getStatues("1335682007179943939");
+    }
+
+    @Test
+    public void testStatusLookup() {
+        ValidatableResponse response = this.tweetAPIClient.getStatuesLookup("1335700614647590913");
+    }
+
+    @Test
+    public void testPostSRetweet() {
+        this.tweetAPIClient.postStatuesRetweet("1335707850207846401");
+    }
+
+    @Test
+    public void testPostUnRetweetRetweet() {
+        this.tweetAPIClient.postUnRetweet("1335719289152802819");
+    }
+
+    @Test
+    public void testGetRetweet() {
+        this.tweetAPIClient.getStatusRetweet("1335723341894868995");
+    }
+
+    @Test
+    public void testGetRetweetOfMe() {
+        this.tweetAPIClient.getStatusRetweetOfMe("1335723341894868995");
+    }
+
+    @Test
+    public void testCreateFavorite() {
+        this.tweetAPIClient.postCreateFavorite("1335735408479662080");
+    }
+
+    @Test
+    public void testFavoriteDelete() {
+        this.tweetAPIClient.postDeleteFavorite("1335735408479662080");
+    }
+
+    @Test
+    public void testFavoriteList() {
+        this.tweetAPIClient.getFavoriteLists("1335735408479662080");
+    }
+
+    @Test
+    public void testPostStatues() {
+        this.tweetAPIClient.postStatuesUpdate("how are you?");
+    }
+
+    @Test
+    public void testTimeLine() {
+        ValidatableResponse response = this.tweetAPIClient.getStatuesHome("");
+    }
+
+    @Test
+    public void testGetMentionsTimeLine() {
+        ValidatableResponse response = this.tweetAPIClient.getStatuesMentions();
+    }
+
+    @Test
+    public void testGetUserTimeLine() {
+        ValidatableResponse response = this.tweetAPIClient.getStatuesUserTimeLine();
+    }
+
+    @Test
+    public void testStatusFilter() {
+        ValidatableResponse response = this.tweetAPIClient.getFilter();
+    }
+
+    @Test
+    public void testStatusSimple() {
+        ValidatableResponse response = this.tweetAPIClient.getStatuesSimple();
+    }
+
+    @Test
+    public void testCollectionEntries() {
+        ValidatableResponse response = this.tweetAPIClient.getCollectionsEntries("");
+    }
+
+    @Test
+    public void testCreateMessage() throws FileNotFoundException {
+        ValidatableResponse response = this.tweetAPIClient.createMessage();
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+    }
+
+    @Test
+    public void testCreateSecondMessage() throws FileNotFoundException {
+        ValidatableResponse response = this.tweetAPIClient.createSecondMessage();
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+    }
+
+    @Test
+    public void testAccountSetting() {
+        ValidatableResponse response = this.tweetAPIClient.getAccountSetting();
+        response.statusCode(200);
+    }
+
+    @Test
+    public void testAccountVerify() {
+        ValidatableResponse response = this.tweetAPIClient.getAccountVerifyCredentials();
+        response.statusCode(200);
+    }
+
+    @Test
+    public void testUserBanner() {
+        long userId = 6253282;
+        ValidatableResponse response = this.tweetAPIClient.getUsersProfileBanner(userId);
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+    }
+
+    @Test
+    public void testAccountRemove() {
+        ValidatableResponse response = this.tweetAPIClient.postRemoveAccount();
+        response.statusCode(200);
+    }
+
+    @Test
+    public void testAccountSettings() {
+        ValidatableResponse response = this.tweetAPIClient.postAccountSetting();
+        response.statusCode(200);
+    }
+
+    @Test
+    public void testAccountUpdateProfile() {
+        ValidatableResponse response = this.tweetAPIClient.postAccountUpdateProfile();
+        response.statusCode(200);
+    }
+
+    @Test
+    public void testSaveSearchesLists() {
+        ValidatableResponse response = this.tweetAPIClient.postSavedSearchesList();
+        response.statusCode(200);
+    }
+
+    @Test
+    public void testSaveSearchesListsShow() {
+        ValidatableResponse response = this.tweetAPIClient.postSavedSearch("I love sandwiches");
+        response.statusCode(200);
+    }
+
+    @Test
+    public void testSaveSearchesDelete() {
+        ValidatableResponse response = this.tweetAPIClient.postSavedSearchesDelete("1335828230096510977");
+        response.statusCode(200);
+    }
+
+    @Test
+    public void testUploadProfileImage() {
+        ValidatableResponse response = this.tweetAPIClient.uploadProfileImage(BaseUpload.cuteBalloons());
+        response.statusCode(200);
+    }
+
+    @Test
+    public void testImageUpload() {
+        ValidatableResponse response = this.tweetAPIClient.uploadPostImage(UploadImage.prettyBeord());
+        response.assertThat().statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+
+    }
+
+    @Test
+    public void testWelcomeMessage() throws FileNotFoundException {
+        ValidatableResponse response = this.tweetAPIClient.createWelcomeMessage(WelcomeMessageSadia.postWelcomeMessage(), WelcomeMessageSadia.flowerImage());
+        response.statusCode(200);
+        System.out.println(response.extract().body().asPrettyString());
+    }
 
 }
-
